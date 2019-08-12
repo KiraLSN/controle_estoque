157a83bde0acce515789c86fb6391f9c68a8c3ca
Luciano Soares Nunes <lucsoaam@gmail.com>
2019-08-11 20:44:19 -0400

    Inclusão do repositório

---
+++ bd.php
@@ -0,0 +1,16 @@
+<?php
+$valida[admin] = "acessorestrito";
+?><?php
+?><?php
+$valida[eu] = "eu";
+?><?php
+$valida[ele] = "ele";
+?><?php
+$valida[gg] = "gg";
+?><?php
+$valida[nn] = "nn";
+?><?php
+$valida[] = "";
+?><?php
+$valida[henry] = "1234";
+?>
